e6c3a9718c83c2d24fff1044769e40862751f6f5
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-11-13 18:16:38 -0800

    fixed tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Character from './components/Character'
+import styled from 'styled-components';
 import axios from 'axios';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,29 +10,30 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  const [data, setData] = useState(null);
-
+  const [data, setData] = useState([]);
   useEffect(() => {
+    const fetchData = () => {
       const URL = 'https://swapi.dev/api/people/';
       axios.get(URL)
            .then((res) => {
-             const characterData = res.data.results;
+             const characterData = res.data;
              setData(characterData)
            })
            .catch((err) => {
              console.log(`Error! ${err}`)
            })
+    }
+    fetchData()
   }, []);
 
 
-  return (
+ return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      {data ? data.map((character, i) => (
-        <Character characterInfo={character} key={i} />
-      )) : <h3>Loading...</h3>}
+        { data ? data.map((character, i) => {
+          return <Character characterInfo={character} key={i}/>
+        }) : <h3>Pending...</h3>}
     </div>
-  );
-}
+  );}
 
 export default App;
