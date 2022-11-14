91217e1c91aa54264d2e7ec9a51fb337ab071827
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-11-13 17:31:33 -0800

    feat: Character styled component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,10 +1,22 @@
 // Write your Character component here
+
+import styled from 'styled-components';
+
 const Character = (props) => {
+  const CharacterCard = styled.div`
+     border-radius: 10px;
+     width: 600px;
+     background-color: white;
+     padding: 10px 20px;
+     margin: 15px;
+   `
   const {characterInfo} = props;
+  if (!characterInfo) return <h2>Loading...</h2>
   return (
-    <div>
+    <CharacterCard>
       <h2>{characterInfo.name}</h2>
-    </div>
+    </CharacterCard>
+
   )
 }
 
